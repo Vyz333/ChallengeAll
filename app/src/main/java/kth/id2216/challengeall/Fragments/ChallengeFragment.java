23ddf47a191224ee49e0dfaae32d57b84fa7f03b
simpelito <simonciesluk@gmail.com>
2016-02-18 12:00:13 +0100

    Add challenge page, not done yet

---
+++ app/src/main/java/kth/id2216/challengeall/Fragments/ChallengeFragment.java
@@ -0,0 +1,63 @@
+package kth.id2216.challengeall.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kth.id2216.challengeall.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChallengeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChallengeFragment extends Fragment {
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+    public ChallengeFragment() {
+        // Required empty public constructor
+    }
+
+    public static ChallengeFragment newInstance(int columnCount) {
+        ChallengeFragment fragment = new ChallengeFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_challenge, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
